96c53025c2c09c7ec9af12a90ab364dfae6d85c2
mcabrex <mrcabardo@gmail.com>
2018-10-09 05:28:13 -0400

    took out the blank carousel items for deploy

--- src/components/Theory/Theory.js
+++ src/components/Theory/Theory.js
@@ -8,34 +8,43 @@ export default class TheoryPage extends Component {
     return (
       <div className="theory">
         <br />
-        <TheoryMode className="mode"/>
+        <TheoryMode className="mode" />
         <TheoryKey />
         <br />
-        <div id="carouselExampleControls" className="carousel slide" data-interval={false}>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-interval={false}
+        >
           <div className="carousel-inner">
             <div className="carousel-item active">
               <div className="scales d-flex align-content-stretch flex-wrap">
                 {scaleBuilder(props.scale)}
               </div>
             </div>
-            <div className="carousel-item">
-            2
-            </div>
-            <div className="carousel-item">
-            3
-            </div>
+            {/* <div className="carousel-item" />
+            <div className="carousel-item" /> */}
           </div>
 
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
             <span className="sr-only">Previous</span>
           </a>
-          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
             <span className="sr-only">Next</span>
           </a>
         </div>
-
       </div>
     );
   }
